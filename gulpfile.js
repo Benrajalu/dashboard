691da7757bef96c19b0d30c8fb6aa4125ac1d5cf
Benoit Rajalu <rajalubenoit@gmail.com>
2015-08-27 16:25:55 +0200

    Base commit - Base structure - Gulpfile that runs a server - Server that runs a live reload ops

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp');
+//var watch = require('gulp-watch');
+var compass = require('gulp-compass');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var liveReload = require('gulp-livereload');
+var plumber = require('gulp-plumber');
+var debug = require('gulp-debug');
+var browserSync = require('browser-sync').create();
+
+//scripts
+gulp.task("scripts", function() {
+	return gulp.src('js/**/*.js')
+		.pipe(debug({title: 'scripts:'}))
+		.pipe(plumber())
+		.pipe(browserSync.stream());
+});
+
+//styles
+gulp.task("styles", function(cb) {
+	return gulp.src('scss/**/*.scss')
+		.pipe(debug({title: 'styles:'}))
+		.pipe(plumber())
+		.pipe(compass({
+			config_file: './config.rb',
+			css: 'css',
+			sass: 'scss',
+		})).on('error', cb)
+		.pipe(browserSync.stream());
+});
+
+//images
+gulp.task("images", function() {
+	return gulp.src('img/**/*.{jpg,jpeg,png,gif}')
+		.pipe(debug({title: 'image:'}))
+		.pipe(plumber())
+		.pipe(browserSync.stream());
+});
+
+// Server
+gulp.task('serve', ['scripts', 'styles'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("js/*.js", ['scripts']);
+    gulp.watch("scss/**/*.scss", ['styles']);
+    gulp.watch("img/**/*.{jpg,jpeg,png,gif}", ['images']);
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+});
+
+
+
+
+//fonts
+gulp.task("fonts", function() {
+	return gulp.src('assets/fonts/**/*.{svg,ttf,eot,woff}')
+		.pipe(debug({title: 'fonts:'}))
+		.pipe(plumber())
+		.pipe(liveReload());
+});
+
+
+gulp.task("default", ["styles", "scripts", "images", "fonts"]);
+
+gulp.task("watch", function() {
+	liveReload.listen({ start:true });
+	gulp.watch('assets/scss/**/*.scss', ['styles']);
+	gulp.watch('assets/js/**/*.js', ['scripts']);
+	gulp.watch('assets/fonts/**/*.{jsvg,ttf,eot,woff}', ['fonts']);
+	gulp.watch('assets/images/**/*.{jpg,jpeg,png,gif}', ['images']);
+});
+
